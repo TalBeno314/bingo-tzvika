61a3a981495b3c319a662142a661daca6e1e5998
TalBeno314 <63107280+TalBeno314@users.noreply.github.com>
2021-11-09 18:07:41 +0200

    adding even more sentences

--- sentence.js
+++ sentence.js
@@ -5,9 +5,11 @@ class Sentence {
     }
 }
 
+//I
+
 const sentence = [
     new Sentence("הומואים ולסבים"),
-    new Sentence("היי וואוו!!"),
+    new Sentence("וואוו!!!"),
     new Sentence("מה זה הרעשים האלה"),
     new Sentence("מה בלעת פטיפון??"),
     new Sentence("יונתן בן משה במילעל"),
@@ -16,12 +18,21 @@ const sentence = [
     new Sentence("יש לי חבר שהוא"),
     new Sentence("חתונות אזרחיות"),
     new Sentence("בן גביר"),
-    new Sentence("הסתכלתי לו בלבן של העיניים!!"),
+    //new Sentence("הסתכלתי לו בלבן של העיניים!!"),
     new Sentence("י'1"),
     new Sentence("ביבי"),
     new Sentence("מאה שערים"),
     new Sentence("אתה לא מבין שאני מדבר"),
-    new Sentence("לרצוח את כל הערבים זה לא פרקטי, צריך להשאיר כמה")
+    new Sentence("לרצוח את כל הערבים זה לא פרקטי, צריך להשאיר כמה"),
+    new Sentence("למה יש פה דיבורים"),
+    new Sentence("קרובי משפחה"),
+    new Sentence("לא לא לא לא לא"),
+    new Sentence("מה קורה שם?"),
+    new Sentence("דיבורי צה\"ל"),
+    new Sentence("אפשר להמשיך?"),
+    new Sentence("כל אחד שיעשה מה שהוא רוצה")
 ];
 
 let queue = [];
+
+//B
